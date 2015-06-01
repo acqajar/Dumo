d7a46d03705399fe3d5c277c4e5e3a71b1a6fe0d
Arsames Qajar <acqajar@gmail.com>
2015-06-01 01:03:59 -0700

    sign-in works, with profile image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
 	def configure_permitted_parameters
 	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :avatar) }
-	  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+	  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
 	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :avatar, :current_password) }
 	end
 
