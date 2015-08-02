8a2dc7e7aea05156f6f01a9eb6f9a5848d5fe475
Arsames Qajar <acqajar@gmail.com>
2015-08-02 14:49:53 -0700

    added users edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601080222) do
+ActiveRecord::Schema.define(version: 20150802205407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 20150601080222) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.text     "content"
+    t.string   "tagline"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
