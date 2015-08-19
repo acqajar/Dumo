2477c7635e6a0b4930682a05635aa870a607be41
Arsames Qajar <acqajar@gmail.com>
2015-08-18 22:18:28 -0700

    amazon s3 heroku fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,23 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = false
 
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY']
-  }
+
+config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_host_name => 's3-us-west-2.amazonaws.com',
+      :s3_protocol => :https,
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY']
+      }
+
+  # config.paperclip_defaults = {
+  # :storage => :s3,
+  # :s3_credentials => {
+  #   :bucket => ENV['S3_BUCKET_NAME'],
+  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #   :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY']
+  # }
 }
 end
