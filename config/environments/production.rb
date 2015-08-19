8e3fc6f8fd088fe198e696eef44332f223c60991
Arsames Qajar <acqajar@gmail.com>
2015-08-18 18:29:14 -0700

    fixing amazon s3 errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.initialize_on_precompile = false
+
+  
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY']
   }
 }
 end
