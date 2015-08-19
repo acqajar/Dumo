e13d2a9707cdbdfb5622b0ab19f670e6ef81673b
Arsames Qajar <acqajar@gmail.com>
2015-08-18 18:55:26 -0700

    testing amazon s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
 
   config.assets.initialize_on_precompile = false
 
-  
+
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_ACCESS_SECRET_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
